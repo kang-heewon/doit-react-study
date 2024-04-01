dc2bae2ddfde68905cd51bae7dd3998df7ceb989
kang-heewon <heewon.dev@gmail.com>
2024-04-01 18:12:26 +0900

    1주차 과제

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
+import { useState } from 'react'
 import './App.css'
 
 function App() {
+  const [isBlack, setIsBlack] = useState(false);
 
   return (
     <div>
-     
+      <div>
+        <h1 className={isBlack ? 'black': 'white'}>안녕하세요.</h1>
+      </div>
+       
+       <button onClick={() => setIsBlack((prev) => !prev)}>반전시키기</button>
     </div>
   )
 }
