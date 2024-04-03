5b472c5dc48e60d085057fa00d43276b5915bee9
kang-heewon <heewon.dev@gmail.com>
2024-04-03 20:12:26 +0900

    2주차 과제

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,11 @@ import './App.css'
 
 function App() {
   const [isBlack, setIsBlack] = useState(false);
+  const [homeworks, setHomeworks] = useState(["e.자료구조", "d.수학", "c.확률과통계", "b.선형대수", "a.물리학"])
+
+  const sort = () => {
+    setHomeworks([...homeworks.sort()])
+  }
 
   return (
     <div>
@@ -10,7 +15,10 @@ function App() {
         <h1 className={isBlack ? 'black': 'white'}>안녕하세요.</h1>
       </div>
        
-       <button onClick={() => setIsBlack((prev) => !prev)}>반전시키기</button>
+      <button onClick={() => setIsBlack((prev) => !prev)}>반전시키기</button>
+      {homeworks.map((homework) => <div key={homework}>{homework}</div>)}
+      <button onClick={sort}>정렬</button>
+
     </div>
   )
 }
