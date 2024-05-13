bc33b47650de113652dfb75ec8f8bce8bea97be2
Owen <heewon.dev@gmail.com>
2024-05-13 16:34:23 +0900

    3주차 과제

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
 import { useState } from 'react'
 import './App.css'
+import { Modal } from './Modal';
 
 function App() {
   const [isBlack, setIsBlack] = useState(false);
+<<<<<<< Updated upstream
+=======
+  const [isModalOpen, setIsModalOpen] = useState(false);
+>>>>>>> Stashed changes
   const [homeworks, setHomeworks] = useState(["e.자료구조", "d.수학", "c.확률과통계", "b.선형대수", "a.물리학"])
 
   const sort = () => {
@@ -16,11 +21,18 @@ function App() {
       </div>
        
       <button onClick={() => setIsBlack((prev) => !prev)}>반전시키기</button>
+<<<<<<< Updated upstream
       {homeworks.map((homework) => <div key={homework}>{homework}</div>)}
       <button onClick={sort}>정렬</button>
 
+=======
+      {homeworks.map((homework) => <div key={homework}>{homework}<button onClick={() => setIsModalOpen((prev) => !prev)}>open</button></div>)}
+      <button onClick={sort}>정렬</button>
+
+      {isModalOpen && <Modal/>}
+>>>>>>> Stashed changes
     </div>
   )
 }
 
-export default App
+export default App;
